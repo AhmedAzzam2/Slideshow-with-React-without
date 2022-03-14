9562c0b9107fda38b4cc192cd4b7d6d02fef8c71
Your Name <you@example.com>
2022-03-14 09:46:56 +0200

    Slideshow with React without

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28"];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="slideshow">
+      <div className="slideshowSlider">
+        {colors.map((backgroundColor, index) => (
+          <div className="slide" key={index} style={{ backgroundColor }}/>
+        ))}
+      </div>
     </div>
   );
 }
